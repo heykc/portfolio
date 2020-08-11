3d0e8eb8bc9787d74bb89be56bd5d76384f3e7e6
Keith Charles <keithc@ouraysportswear.com>
2020-08-10 23:26:24 -0600

    added hambuger click func

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-window.onload = () => {
-  document.querySelector(".bar").style.marginRight = 0;
-};
+
+let nav = document.querySelector(".nav .container");
+
+let hamburger = document.querySelector(".nav .ham");
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle("expanded")
+})
 
 window.addEventListener("scroll", function (e) {
   const targets = document.querySelectorAll(".para");
-  
-  for(const t of targets) {
+
+  for (const t of targets) {
     const offset = e.currentTarget.pageYOffset;
     const rateY = offset * (+t.dataset.speedY || 0);
     const rateX = offset * (+t.dataset.speedX || 0);
-    console.log(rateX, rateY)
+    console.log(rateX, rateY);
     t.style.transform = `translate3d(${rateX}px, ${rateY}px, 0px)`;
   }
 });

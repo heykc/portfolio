1403f0ab339a00b80aa446a9b62b1ba392a4d7f3
Keith Charles <keithc@ouraysportswear.com>
2020-09-03 23:40:23 -0600

    created social svgs

---
+++ src/svgs/web.js
@@ -0,0 +1,21 @@
+import React from 'react';
+export default function Web() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      height="1em"
+      width="1em"
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="2" y1="12" x2="22" y2="12"></line>
+      <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 
+        0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
+      />
+    </svg>
+  );
+}

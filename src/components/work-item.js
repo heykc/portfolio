c0b05dbed1a38bc934303158962706117872bd60
Keith Charles <keithc@ouraysportswear.com>
2020-09-04 10:08:07 -0600

    fixed work-item load issue

--- src/components/work-item.js
+++ src/components/work-item.js
@@ -2,7 +2,7 @@ import React from "react";
 import WIP from "../svgs/wip";
 
 export default function WorkItem(props) {
-  const [info, setInfo] = React.useState(null)
+  const [info, setInfo] = React.useState({})
 
   React.useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/static/data/work.json`)
@@ -14,33 +14,32 @@ export default function WorkItem(props) {
   }, [])
   
   return (
-    info &&
-      <div className={
-        `work-item ${
-          props.isShowing ? "show" : ""
-        } ${
-          props.rev ? "rev" : ""
-        }`
-      }>
-        <section  id={props.slug} className="row">
-          <div className="img col">
-            {info.img 
-              ? (<img src={ process.env.PUBLIC_URL + '/static/images' + info.img} />) 
-              : "No Image"
-            }
-          </div>
-          <article className="col row c">
-            <div className={`circ ${props.color}`}></div>
-            <header>
-              {props.wip && <WIP />}
-              <h3>{info.title ? info.title : `No Title`}</h3>
-            </header>
-            <p>
-              {info.desc ? info.desc : "No Description"}
-            </p>
-            {info.links && info.links.map((link, i) => <button key={`work-btn-${i}`} className="secondary col top"><a href={link.link} target="_blank">{link.text} &gt;</a></button>)}
-          </article>
-        </section>
-      </div>
+    <div className={
+      `work-item ${
+        props.isShowing ? "show" : ""
+      } ${
+        props.rev ? "rev" : ""
+      }`
+    }>
+      <section  id={props.slug} className="row">
+        <div className="img col">
+          {info.img 
+            ? (<img src={ process.env.PUBLIC_URL + '/static/images' + info.img} />) 
+            : ""
+          }
+        </div>
+        <article className="col row c">
+          <div className={`circ ${props.color}`}></div>
+          <header>
+            {props.wip && <WIP />}
+            <h3>{info.title ? info.title : ""}</h3>
+          </header>
+          <p>
+            {info.desc ? info.desc : ""}
+          </p>
+          {info.links && info.links.map((link, i) => <button key={`work-btn-${i}`} className="secondary col top"><a href={link.link} target="_blank">{link.text} &gt;</a></button>)}
+        </article>
+      </section>
+    </div>
   )
 }

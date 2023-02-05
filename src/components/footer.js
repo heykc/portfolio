f21f33424c3a0cafeb68d83e5dce543371dbedae
Keith Charles <keith.k.charles@gmail.com>
2023-02-04 17:59:59 -0700

    update copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer () {
   return (
     <footer>
-      <p className="copy">&copy;Keith Charles 2020</p>
+      <p className="copy">&copy;Keith Charles 2023</p>
     </footer>
   )
 }
